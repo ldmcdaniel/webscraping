bbf35e3676aafbad97ff17f4b1ac1b2b13250d6a
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-09-14 22:13:36 -0500

    updated commit

--- stringparse.js
+++ stringparse.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var cheerio = require('cheerio');
 var request = require('request');
-
+var userRequest = 'flexbox';
+var browserName = 'chrome';
+var browserVersion;
 console.log()
 
 http.createServer(function(req, res) {
@@ -14,18 +16,21 @@ http.createServer(function(req, res) {
   // console.log(userAgentURL);
 
   request.get(userAgentURL, function (err, xhr, body) {
-    var jso= JSON.parse(body);
-    var result = jso.agent_name + ' ' + jso.agent_version;
-    // console.log(result);
+    var browser = JSON.parse(body);
+    browserName = browser.agent_name;
+    browserVersion = parseInt(browser.agent_version)
+    var result =  browserName+ ' ' + browserVersion;
+    console.log(result);
   });
 
   request.get('http://caniuse.com/flexbox', function (err, xhr, body) {
     // var char = cheerio.find('chrome');
     // console.log(body);
     var $ = cheerio.load(body);
-    var browser = $(body).find('.browser--chrome').next().children().each(function () {
-      if($(this).text() == "44") {
-        console.log($(this).attr('title'))
+    var browser = $(body).find('.browser--chrome' + browserName).next().children().each(function () {
+      if($(this).text() == "44" ) {
+        var result = $(this).attr('title').split('-')[1].trim();
+        console.log(result)
       }
     });
     // console.log(browser);
